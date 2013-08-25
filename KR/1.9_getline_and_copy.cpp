0f3f31063fc9aba3c28968afe07510ef746487c3
Demi Wang <wangdx.05@gmail.com>
2013-08-25 15:18:23 -0500

    KR_HW_2.2

--- KR/1.9_getline_and_copy.cpp
+++ KR/1.9_getline_and_copy.cpp
@@ -44,6 +44,6 @@ void copy(char to[], char from[])
 	int i;
 
 	i = 0;
-	while ((to[i] = from[i]) != '/0')
+	while ((to[i] = from[i]) != '\0')
 		++i;
 }
---
+++ KR/Q_2.2_getline_and_copy.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define MAXLINE 1000	/* maximum input line size */
+
+int getline(char line[],int maxline);
+void copy (char to[], char from[]);
+
+/* print longest input line */
+main()
+{
+	int len;	/* current line length */
+	int max;	/* maximum length seen so far */
+	char line[MAXLINE];		/* current input line */					
+	char longest[MAXLINE];	/* longest line saved here */
+
+	max=0;
+	while ((len = getline(line, MAXLINE))>0)
+		if (len > max) {
+			max =len;
+			copy(longest, line);
+		}
+	if (max > 0)
+		printf("%s", longest);
+	return 0;
+}
+
+/* getline: read a line into s, return length */
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	if (c=getchar()!=EOF){
+		if(c!='\n'){
+			i=0;
+			if(i<lim-1){
+				s[i]=c;
+			}
+
+		}
+	}
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	} 
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
