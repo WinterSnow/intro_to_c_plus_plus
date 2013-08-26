0c9266c5d9a3696f888c3aedacb18afc614f7546
Demi Wang <wangdx.05@gmail.com>
2013-08-25 20:54:50 -0500

    Convert s to integer

---
+++ KR/2.7_atoi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std; 
+
+int atoi(char s[]);
+
+int main() {
+
+	char* s ;
+	cout << "Please enter a number: ";
+	cin >> s ;
+ 
+	cout << atoi(s);
+
+}
+
+/* atoi: convert s to integer */
+int atoi(char s[])
+{
+	int i, n;
+
+	n = 0;
+	for (i = 0; s[i] >= '0' &&  s[i] <= '9'; ++i)
+		n = 10 * n + (s[i] - '0');
+	return n;
+}
