16a9760a0faf5087a2024dbb1dc8f6a4d5692f62
Demi Wang <wangdx.05@gmail.com>
2013-08-25 21:18:22 -0500

    Convert hexadecimal string to interger

---
+++ KR/Q_2.3_htoi.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std; 
+
+int htoi(char s[]);
+
+int main() {
+
+	char* s ;
+	cout << "Please enter a number: ";
+	cin >> s ;
+ 
+	cout << htoi(s);
+
+}
+
+/* atoi: convert hexademical s to integer */
+int htoi(char s[])
+{
+	int i, n, c;
+
+	n = 0;
+
+	for (i = 0; s[i] != '\0'; ++i){
+		if (s[i] >= '0' &&  s[i] <= '9') {
+			c = s[i] - '0';
+		}
+		else if (s[i] >= 'A' && s[i] <= 'F'){
+			c = s[i] - 55;
+		}
+		else if (s[i] >= 'a' && s[i] <= 'f'){
+			c = s[i] - 87;
+		}
+		else if (s[i] == 'x' || s[i] == 'X'){
+			c = 0;
+		}
+		n = 16 * n + c;
+	}
+	return n;
+}
