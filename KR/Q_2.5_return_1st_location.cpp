4c32cd973d1311eba12032f058b813406f077c52
Demi Wang <wangdx.05@gmail.com>
2013-09-02 18:42:16 -0500

    Return the first location in the string s1 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.

---
+++ KR/Q_2.5_return_1st_location.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+int return_1st_location(char s[], char c[]);
+
+int main() {
+
+	char* c;
+	char* s;
+
+	cout << "Please enter a squeezer string: ";
+	cin.getline(c,100);
+
+	cin.clear(); cin.sync();
+
+	cout << "Please enter the string to be squeezed: ";
+	cin.getline(s,100);
+
+	int i;
+
+	int out = return_1st_location(s, c);
+		
+	cout << "The squeezed result is " << out;
+
+
+}
+
+
+int return_1st_location(char s[], char c[])
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++){
+		for (j = 0; c[j] != '\0'; j++){
+			if (s[i] == c[j]){
+				return i;
+			}
+		}
+	}
+	return -1;
+}
