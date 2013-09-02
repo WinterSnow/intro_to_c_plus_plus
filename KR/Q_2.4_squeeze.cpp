dae800f123062a54c019c026f5b80c14e3125176
Demi Wang <wangdx.05@gmail.com>
2013-09-02 17:21:05 -0500

    Delete each character in S1 that matches any character in the string S2.

---
+++ KR/Q_2.4_squeeze.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+void squeeze(char s[], char c);
+
+int main() {
+
+	char* c;
+	char* s;
+
+	cout << "Please enter a squeezer string: ";
+	cin.getline(c,100);
+
+	cin.clear(); cin.sync();
+
+	cout << "Please enter the string to be squeezed: ";
+	cin.getline(s,100);
+
+	int i;
+	for (i = 0; c[i] !='\0'; i++){
+		squeeze(s, c[i]);
+	}	
+	cout << "The squeezed result is " << s;
+
+}
+
+/* squeeze: delete all c from s */
+void squeeze(char s[], char c)
+{
+	int i, j;
+
+	for (i = j = 0; s[i] != '\0'; i++){
+		if (s[i] != c){
+			s[j++] = s[i];
+		}
+	}
+	s[j] = '\0';
+}
