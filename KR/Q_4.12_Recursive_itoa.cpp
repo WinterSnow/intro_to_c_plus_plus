ffa51a498f6dd3f61c8659e20b1c855fdb5caacf
Demi Wang <wangdx.05@gmail.com>
2014-08-30 18:01:52 -0500

    It's very important to have static value, otherwise I will either get segamentation error because i was not initialized or i got reset to zero everytime the recursive function is called

--- KR/Q_4.12_Recursive_itoa.cpp
+++ KR/Q_4.12_Recursive_itoa.cpp
@@ -1,3 +1,4 @@
+#include <math.h> /* for abs function */
 #include <ctype.h>
 #include <iostream>
 using namespace std; 
@@ -11,22 +12,24 @@ int main() {
     cout << "Please enter a number for n: ";
     cin >> n ;
     recursive_itoa (n,s);
- //   cout << s;
+    cout << s;
+
 }
+
 /* itoa: convert n to characters in s */
 void recursive_itoa (int n, char s[])
 {
-    int i, sign; 
-    
-//    if ((sign = n) < 0){   /* record sign */
-  //      s[0]='-';
-    //    n = -n; /* make n positive */  
- //   if (n/10)
-   //     recursive_itoa (n/10,s);
+    static int i;
 
-    cout<< (n % 10 + '0')<<"\n"; 
-  //  s[i]='\0';
-
-
+    if (n/10){
+        recursive_itoa (n/10,s);
+    }
+    else {
+        i=0;
+        if ( n<0)
+            s[0]='-';
+    }
+    s[i++]=abs(n) % 10 +'0'; 
+    s[i]='\0';
 }
 
