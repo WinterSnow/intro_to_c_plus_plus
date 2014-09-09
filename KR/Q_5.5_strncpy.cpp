72873157e995a5a7e64c94d1c748487171e07262
Demi Wang <wangdx.05@gmail.com>
2014-09-08 21:04:54 -0500

    change function from void to char*

--- KR/Q_5.5_strncpy.cpp
+++ KR/Q_5.5_strncpy.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std; 
 
-void strncpy(char s[], char t[], int n);
+char* strncpy(char s[], char t[], int n);
 
 int main() {
 
@@ -16,17 +16,17 @@ int main() {
     cout << "Please enter a number for n: ";
     cin >> n ;
 
-    strncpy(a,b, n);
-    cout << "s: " << a << '\n';
+    cout << "s: " <<strncpy(a,b, n) <<'\n';
 
 }
 
 /* strcpy: copy t to s; array subscript version */
-void strncpy (char *s, char *t, int n)
+char* strncpy (char *s, char *t, int n)
 {
     int i;
 
     i = 0;
     while ((s[i] = t[i])!= '\0'&& i<n-1)
         i++;
+    return s;
 }
