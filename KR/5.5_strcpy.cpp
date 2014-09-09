487eaaf0a5e8739d83e15edd6427ca13a8ecab00
Demi Wang <wangdx.05@gmail.com>
2014-09-08 20:38:25 -0500

    copy t to s; array subscript version

---
+++ KR/5.5_strcpy.cpp
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <iostream>
+using namespace std; 
+
+void strcat_pointer(char s[], char t[]);
+
+int main() {
+
+    char a[256];
+    char *b= new char [256];
+
+    cout << "Please enter a string for b: ";
+    cin >> b ;
+
+    strcpy(a,b);
+    cout << "s: " << a << '\n';
+
+}
+
+/* strcpy: copy t to s; array subscript version */
+void strcpy (char *s, char *t)
+{
+    int i;
+
+    i = 0;
+    while ((s[i] = t[i])!= '\0')
+        i++;
+}
